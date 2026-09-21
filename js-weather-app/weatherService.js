9f4f8a6e8999a827b897f2a09297471e7b8d848b
Leonie Steponaitis <xluxeo@gmx.de>
2026-09-21 08:20:35 +0200

    Add files via upload

---
+++ js-weather-app/weatherService.js
@@ -0,0 +1,31 @@
+/*WeatherService-Modul erstellen:
+- Funktion zum Abrufen aktueller Wetterdaten für eine Stadt
+- Funktion zum Formatieren der API-Antwort
+- Verwendung von Named Exports
+
+API verwenden:
+- Nutze die kostenlose OpenWeatherMap API
+- Implementiere Fetch-Aufrufe mit async/await
+- Behandle Fehler (ungültige Stadt, Netzwerkfehler*/
+
+export async function fetchWeather(location) {
+  const response = await fetch(`/api/weather?city=${encodeURIComponent(location)}`);
+
+  if (!response.ok) {
+    throw new Error(response.status === 404 ? "Stadt nicht gefunden" : "Server-Fehler");
+  }
+
+  return await response.json();
+}
+
+export function formatResponse(data) {
+  return {
+    name: data.name,
+    temperature: `${Math.round(data.main.temp)}°C`,
+    description: data.weather[0].description,
+    icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+    humidity: `Luftfeuchtigkeit: ${data.main.humidity}%`,
+    wind: `Windgeschwindigkeit: ${data.wind.speed}m/s`,
+    country: `Land: ${data.sys.country}`
+  }
+}
